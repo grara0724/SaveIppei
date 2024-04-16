57fe36d4394b0822e867cf1e666b53b1760e6150
grara0724 <kazuma.xx24@gmail.com>
2024-04-17 00:10:04 +0900

    moneyクラスをどのように組み込んでいけばいいかがわからなかったので、いったん文書生成に逃げたら楽しくてそれで終わりました。

--- SaveIppei/SavaIppei/Money.java
+++ SaveIppei/SavaIppei/Money.java
@@ -1,4 +1,22 @@
 
 public class Money {
+	private int handmoney;
+	private int betmoney;
+
+	public int getHandmoney() {
+		return handmoney;
+	}
+
+	public void setHandmoney(int handmoney) {
+		this.handmoney = handmoney;
+	}
+
+	public int getBetmoney() {
+		return betmoney;
+	}
+
+	public void setBetmoney(int betmoney) {
+		this.betmoney = betmoney;
+	}
 
 }
